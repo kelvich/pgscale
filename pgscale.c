2a1cd40552b1160535f67767356993742abf6f2a
Stas Kelvich <stas.kelvich@gmail.com>
2016-05-10 22:19:02 +0300

    parse http method

--- pgscale.c
+++ pgscale.c
@@ -25,6 +25,7 @@ PG_MODULE_MAGIC;
 PG_FUNCTION_INFO_V1(pgscale_start);
 PG_FUNCTION_INFO_V1(pgscale_stop);
 
+void _PG_init(void);
 void pgscale_http_main(Datum arg) pg_attribute_noreturn();
 
 static BackgroundWorker pgscale_http_worker = {
@@ -32,67 +33,124 @@ static BackgroundWorker pgscale_http_worker = {
 	BGWORKER_SHMEM_ACCESS | BGWORKER_BACKEND_DATABASE_CONNECTION,
 	BgWorkerStart_RecoveryFinished,
 	BGW_NEVER_RESTART,
-	NULL,
-	"pgscale",
-	"pgscale_http_main"
+	pgscale_http_main
 };
+
+void
+_PG_init(void)
+{
+	// XXX: add some connection info to GUC variables
+	RegisterBackgroundWorker(&pgscale_http_worker);
+}
 
 static void
 pgscale_sigterm(SIGNAL_ARGS)
 {
+	// XXX: sometimes I can't bind to socket some time after restart
+	// probably data on socket (if some) should be read before exit.
 	proc_exit(0);
 }
 
-static void
-pgscale_run_sql(char *sql, StringInfo response) {
+static int
+pgscale_run_sql(char *pg_view_name, StringInfo response) {
+	char select_sql[60] = "select * from ";
 	int ret;
+
+	if (strlen(pg_view_name) > 40)
+		return -1;
+
+	(void) strncat(select_sql, pg_view_name, strlen(pg_view_name));
 
 	SetCurrentStatementStartTimestamp();
 	StartTransactionCommand();
 	SPI_connect();
 	PushActiveSnapshot(GetTransactionSnapshot());
 
-	ret = SPI_execute(sql, true, 0);
+	ret = SPI_execute(select_sql, true, 0);
 
 	if (ret != SPI_OK_SELECT)
 		elog(FATAL, "SPI_execute failed: error code %d", ret);
 
 	if (SPI_tuptable != NULL)
 	{
-	    TupleDesc tupdesc = SPI_tuptable->tupdesc;
-	    SPITupleTable *tuptable = SPI_tuptable;
-	    int proc = SPI_processed;
-	    int i, j;
+		TupleDesc tupdesc = SPI_tuptable->tupdesc;
+		SPITupleTable *tuptable = SPI_tuptable;
+		int proc = SPI_processed;
+		int i, j;
 
-	    appendStringInfo(response, "[");
-	    for (j = 0; j < proc; j++)
-	    {
-	        HeapTuple tuple = tuptable->vals[j];
+		appendStringInfo(response, "[");
+		for (j = 0; j < proc; j++)
+		{
+			HeapTuple tuple = tuptable->vals[j];
 
-	        appendStringInfo(response, "{");
-	        for (i = 1; i <= tupdesc->natts; i++)
-	            appendStringInfo(response,
-	            	"%s : %s, ", SPI_fname(tupdesc, i), SPI_getvalue(tuple, tupdesc, i));
-	        appendStringInfo(response, "}, ");
-	    }
-	    appendStringInfo(response, "]");
+			appendStringInfo(response, "{");
+			for (i = 1; i <= tupdesc->natts; i++)
+				appendStringInfo(response,
+					"%s : %s, ", SPI_fname(tupdesc, i), SPI_getvalue(tuple, tupdesc, i));
+			appendStringInfo(response, "}, ");
+		}
+		appendStringInfo(response, "]");
 	}
 
 	SPI_finish();
 	PopActiveSnapshot();
 	CommitTransactionCommand();
 	pgstat_report_activity(STATE_IDLE, NULL);
+
+	return 0;
 }
 
 static StringInfo
 pgscale_http_handle_request(char *buf, int len) {
-	char *sql = "select * from pg_stat_database";
 	StringInfo response = makeStringInfo();
 
-	appendStringInfo(response, "HTTP/1.1 200 OK\r\n\r\n");
-	pgscale_run_sql(sql, response);
-	appendStringInfo(response, "\r\n\r\n");
+	/* rfc2616 grammar tokens */
+	char *request_line, *method, *request_uri; 
 
+	/*
+	 * We are interested only in Request-Line out of all Request, so skip
+	 * following headers and possible message-body.
+	 */
+	request_line = strsep(&buf, "\r\n");
+	// XXX: add some checks for malformed request
+	fprintf(stderr, "request_line: '%s'\n", request_line);
+
+	method = strsep(&request_line, " ");
+	request_uri = strsep(&request_line, " ");
+
+	fprintf(stderr, "method: '%s'\n", method);
+	fprintf(stderr, "request_uri: '%s'\n", request_uri);
+
+	/* Reply with 'Not Implemented' for any other method except GET */
+	if (strcmp(method, "GET"))
+	{
+		appendStringInfo(response, "HTTP/1.1 501 Not Implemented\r\n\r\n");
+		return response;
+	}
+
+	/*
+	 * Catch request to specified scope and return corresponding view output
+	 * as json.
+	 */
+	if (!strncmp(request_uri, "/pgstat/", 8))
+	{
+		char *pg_view_name = request_uri + 8;
+		// int ret;
+
+		// fnmatch here
+
+		// XXX: add here whitelist of sql views
+		// XXX: add error handling in pgscale_run_sql() (?)
+		appendStringInfo(response, "HTTP/1.1 200 OK\r\n\r\n");
+		pgscale_run_sql(pg_view_name, response);
+		appendStringInfo(response, "\r\n\r\n");
+		return response;
+	}
+
+	/*
+	 * Here we assume that request_uri is path to file.
+	 */
+	appendStringInfo(response, "HTTP/1.1 404 Not found\r\n\r\n");
 	return response;
 }
 
@@ -133,7 +191,6 @@ pgscale_http_main(Datum arg)
 	}
 
 	while (1) {
-		// StringInfoData msg;
 		StringInfo msg = makeStringInfo();
 		char buf[REQEST_MAX_SIZE];
 		int bytes_sent, bytes_recv;
@@ -151,36 +208,17 @@ pgscale_http_main(Datum arg)
 			printf("Can't receive. %s\n", strerror(errno));
 		}
 
+		fprintf(stderr, ">>>>>>>>>\n%.*s>>>>>>>>>\n", bytes_recv, buf);
 		msg = pgscale_http_handle_request(buf, bytes_recv);
-		bytes_sent = send(cli_fd, msg->data, msg->len, 0);
-		
-		pfree(msg->data);
+		fprintf(stderr, "<<<<<<<<<\n%s<<<<<<<<<\n\n", msg->data);
 
+		bytes_sent = send(cli_fd, msg->data, msg->len, 0);
+		pfree(msg->data);
 		close(cli_fd);
 	}
 
-	printf("Exiting\n");
 	shutdown(s, SHUT_RDWR);
 	close(s);
 
 	proc_exit(1);
 }
-
-Datum
-pgscale_start(PG_FUNCTION_ARGS)
-{
-	BackgroundWorkerHandle *handle;
-	BgwHandleStatus status;
-	pid_t pid;
-
-	pgscale_http_worker.bgw_notify_pid = MyProcPid;
-	if (!RegisterDynamicBackgroundWorker(&pgscale_http_worker, &handle))
-		fprintf(stderr, "%s\n", "can't register http worker");
-
-	status = WaitForBackgroundWorkerStartup(handle, &pid);
-
-	fprintf(stderr, "pgscale_start called: status=%d, pid=%d\n", status, pid);
-
-	PG_RETURN_INT32(42);
-}
-
